570c3abb3da4f0d58b94057fe92b69590ef5c0d6
diegocr <diegocr@gmail.com>
2016-09-12 15:46:18 +0200

    disable search bar on recents
    Change-Id: Id0a1535b52873ab62bdf45f281b376d2339ce946

--- packages/SystemUI/src/com/android/systemui/recents/Constants.java
+++ packages/SystemUI/src/com/android/systemui/recents/Constants.java
@@ -42,7 +42,7 @@ public class Constants {
             // Enables debug mode
             public static final boolean EnableDebugMode = false;
             // Enables the search bar layout
-            public static final boolean EnableSearchLayout = true;
+            public static final boolean EnableSearchLayout = false;
             // Enables the thumbnail alpha on the front-most task
             public static final boolean EnableThumbnailAlphaOnFrontmost = false;
             // Enables all system stacks to show up in the same recents stack
